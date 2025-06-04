d5493b1d386eaaa63330e0f036ff720fe8f7af4a
PAUL <kimjs20020426@gmail.com>
2025-06-04 15:07:21 +0900

    청년정책 데이터 줄바꿈 적용

--- youth_policy/models.py
+++ youth_policy/models.py
@@ -5,9 +5,9 @@ class YouthPolicy(models.Model):
     description = models.TextField("정책설명")
     age_range = models.CharField("대상연령", max_length=50)
     keyword = models.CharField("정책키워드", max_length=100, blank=True)
-    region = models.CharField("시행지역", max_length=100, blank=True)
-    period = models.CharField("신청기간", max_length=100, blank=True)
-    URL_ADD = models.URLField("신청URL", max_length=100, blank=True)
+    region = models.CharField("시행지역", max_length=200, blank=True)
+    period = models.CharField("신청기간", max_length=200, blank=True)
+    URL_ADD = models.URLField("신청URL", max_length=200, blank=True)
 
     def __str__(self):
         return self.name
