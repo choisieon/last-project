ca6a5d122dbde4c6c961fcdea9a610999deca6b8
PAUL <kimjs20020426@gmail.com>
2025-06-05 17:04:37 +0900

    update

--- community/views.py
+++ community/views.py
@@ -3,7 +3,6 @@ from .models import Post
 
 # Create your views here.
 
-
 def index(request):
     posts = Post.objects.order_by('-created_at')  # 최신순 정렬
     return render(request, 'community/index.html', {'posts': posts})
